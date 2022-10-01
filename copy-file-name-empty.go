10409fdffc99a2f6fa0260959aaf6c39a393f27c
lanastasov <lubomir.anastasov@gmail.com>
2022-10-01 12:33:39 +0300

    CFNE

---
+++ copy-file-name-empty.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fmt.Println("--copy-file-name-empty.go--ver0.0.5")
+
+	files, _ := filepath.Glob("*.mp4")
+	if len(files) == 0 {
+		files, _ = filepath.Glob("*.MP4")
+	}
+	if len(files) > 0 {
+		os.Mkdir("CFNE", 0755 )
+	}
+	for i := range files {
+		path := filepath.Join("CFNE", files[i]);
+		//fmt.Println(files[i])
+		//CFNE = Copy File Name Empty
+		//f, err := os.Create("CFNE\\" + files[i])
+		f, err := os.Create(path)
+		check(err)
+		defer f.Close()
+	}
+
+}
